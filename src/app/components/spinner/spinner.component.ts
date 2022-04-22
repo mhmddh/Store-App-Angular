8f10ae97935fc5c5c92b5aaaf1c240f9717f1c06
mhmddh <mohamad.hsen.daher@gmail.com>
2022-04-22 03:33:18 +0300

    spinner style update

--- src/app/components/spinner/spinner.component.ts
+++ src/app/components/spinner/spinner.component.ts
@@ -1,12 +1,13 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Spinner } from 'src/app/common/models/model';
-
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-spinner',
   templateUrl: './spinner.component.html',
   styleUrls: ['./spinner.component.css']
 })
 export class SpinnerComponent implements OnInit {
+  faCartShopping = faCartShopping;
   @Input() attributes!: Spinner;
 
   constructor() { }
